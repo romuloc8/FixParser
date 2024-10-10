ca37f85d7a4991af1aa20f940cdc66ea6d8c82dd
romuloc8 <zhengzhixuan95@gmail.com>
2024-10-10 00:08:38 -0700

    Removes unused import.

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Main class for testing the parser.
