69edaf5b740e0ceda48ed8d5f4abb582821d12ef
romuloc8 <zhengzhixuan95@gmail.com>
2024-10-10 00:08:17 -0700

    Added test set for performance checking.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,36 @@
+import org.zhengzhixuan.fix.message.FixMessage;
 import org.zhengzhixuan.fix.parser.FixParser;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
+/**
+ * Main class for testing the parser.
+ * With the test set located in performance folder, which has 80 messages.
+ * It takes 0.024s to process these messages, with average 0.0003s per message.
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
-        if (args.length < 1) {
-            throw new IllegalArgumentException("Please provide a path to the FIX message file.");
-        }
+        String path = args.length > 0 ? args[0] : ".\\src\\org\\zhengzhixuan\\fix\\performance\\TESTSET.txt";
         FixParser parser = FixParser.create();
-        Path fixMessagesPath = Paths.get(args[0]);
+        Path fixMessagesPath = Paths.get(path);
         try {
             BufferedReader reader = Files.newBufferedReader(fixMessagesPath, StandardCharsets.US_ASCII);
-            reader.lines()
-                    .map(message -> parser.parse(message.getBytes(StandardCharsets.US_ASCII)))
-                    .forEach(System.out::println);
+            List<String> messages =  reader.lines().toList();
+            Instant start = Instant.now();
+            List<FixMessage> fixMessages =
+                    messages.stream()
+                            .map(message -> parser.parse(message.getBytes(StandardCharsets.US_ASCII))).toList();
+            Instant end = Instant.now();
+            fixMessages.forEach(System.out::println);
+            System.out.println(Duration.between(start, end));
         } catch (IOException e) {
             throw new IOException("Error occurs when reading FIX message file.", e);
         }
